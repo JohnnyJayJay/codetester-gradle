5e9579a44020e4421ef37636a811531d25a0812f
JohnnyJayJay <johnnyjayjay02@gmail.com>
2021-02-28 15:41:40 +0100

    Remove maven publication configuration (already configured by gradle dev plugin)

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,15 +30,3 @@ gradlePlugin {
         }
     }
 }
-
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            groupId = project.group as String
-            artifactId = project.name
-            version = project.version as String
-
-            from(components["java"])
-        }
-    }
-}
